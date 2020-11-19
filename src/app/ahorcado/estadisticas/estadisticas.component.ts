b21d610cd2dcf085a1cc58c5b47e68113b7d5fd6
Alberto gentilcore <gentilcore.ac@hotmail.com>
2020-11-19 00:57:47 -0300

    se comentan lineas sin uso

--- src/app/ahorcado/estadisticas/estadisticas.component.ts
+++ src/app/ahorcado/estadisticas/estadisticas.component.ts
@@ -32,59 +32,59 @@ export class EstadisticasComponent implements OnInit {
   }
 
 
-  ngOnChange(): void{ 
-    this.getLetrasCorrectas();
-    this.getLetrasIncorrectas();
-    this.getIntentosRestantes();
-  }
-  vidas() {
-    switch(this.intentosRestantes) {
-        case 4:
-          this.vidaImagen = this.URL_IMAGENES_PRE+"ahorcadoinicial"+this.URL_IMAGENES_EXT;    
-        break;
-        case 3:    
-        this.vidaImagen = this.URL_IMAGENES_PRE+"ahorcadounfallo"+this.URL_IMAGENES_EXT;    
-        break;
-        case 2: 
-        this.vidaImagen = this.URL_IMAGENES_PRE+"ahorcadodosfallos"+this.URL_IMAGENES_EXT;    
-        break;
-        case 1: 
-        this.vidaImagen = this.URL_IMAGENES_PRE+"ahorcadotresfallos"+this.URL_IMAGENES_EXT;    
-        break;
-        case 0: 
-         this.vidaImagen = this.URL_IMAGENES_PRE+"ahorcadocompleto"+this.URL_IMAGENES_EXT;    
-        //this.gameOver();
-        break;   
-    
-        }
-      }
-
-
-
-  // getPalabra(): void { 
+  // ngOnChange(): void{ 
+  //   this.getLetrasCorrectas();
+  //   this.getLetrasIncorrectas();
+  //   this.getIntentosRestantes();
   // }
+  // vidas() {
+  //   switch(this.intentosRestantes) {
+  //       case 4:
+  //         this.vidaImagen = this.URL_IMAGENES_PRE+"ahorcadoinicial"+this.URL_IMAGENES_EXT;    
+  //       break;
+  //       case 3:    
+  //       this.vidaImagen = this.URL_IMAGENES_PRE+"ahorcadounfallo"+this.URL_IMAGENES_EXT;    
+  //       break;
+  //       case 2: 
+  //       this.vidaImagen = this.URL_IMAGENES_PRE+"ahorcadodosfallos"+this.URL_IMAGENES_EXT;    
+  //       break;
+  //       case 1: 
+  //       this.vidaImagen = this.URL_IMAGENES_PRE+"ahorcadotresfallos"+this.URL_IMAGENES_EXT;    
+  //       break;
+  //       case 0: 
+  //        this.vidaImagen = this.URL_IMAGENES_PRE+"ahorcadocompleto"+this.URL_IMAGENES_EXT;    
+  //       //this.gameOver();
+  //       break;   
+    
+  //       }
+  //     }
+
+
+
+  // // getPalabra(): void { 
+  // // }
   
-  getLetrasIncorrectas(): void {
-    this.ahorcadoService.getLetrasIncorrectas().subscribe({
-      next: res => { 
-        this.letrasIncorrectas = res;        
-      } 
-    });
-  }
-  getLetrasCorrectas(): void {
-    this.ahorcadoService.getLetrasCorrectas().subscribe({
-      next: res => { 
-        console.log(res);
-        this.letrasCorrectas = res;
-      } 
-    });
-  }
-  getIntentosRestantes(): void {
-    this.ahorcadoService.getIntentosRestantes().subscribe({
-      next: res => {  
-        this.intentosRestantes = res;
-        this.vidas();
-      } 
-    });
-  }
+  // getLetrasIncorrectas(): void {
+  //   this.ahorcadoService.getLetrasIncorrectas().subscribe({
+  //     next: res => { 
+  //       this.letrasIncorrectas = res;        
+  //     } 
+  //   });
+  // }
+  // getLetrasCorrectas(): void {
+  //   this.ahorcadoService.getLetrasCorrectas().subscribe({
+  //     next: res => { 
+  //       console.log(res);
+  //       this.letrasCorrectas = res;
+  //     } 
+  //   });
+  // }
+  // getIntentosRestantes(): void {
+  //   this.ahorcadoService.getIntentosRestantes().subscribe({
+  //     next: res => {  
+  //       this.intentosRestantes = res;
+  //       this.vidas();
+  //     } 
+  //   });
+  // }
 }
